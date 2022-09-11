3018a57f58859328766aad48f253f3a9e8391ba2
Arissarasan <cha.baasan@gmail.com>
2022-09-11 12:25:38 +0700

    seventh

--- concatArray.js
+++ concatArray.js
@@ -6,10 +6,10 @@ function concatArray(array1, array2) {
     (array2 === null || array2 === undefined)
   )
     return undefined;
-  else if (array1?.length === 0 && array2?.length === 0) return [];
-  else if (array1 === null || array1 === undefined || array1.length === 0)
+  else if (array1 === [] && array2 === []) return [];
+  else if (array1 === null || array1 === undefined || array1 === [])
     return array2;
-  else if (array2 === null || array2 === undefined || array2.length === 0)
+  else if (array2 === null || array2 === undefined || array2 === [])
     return array1;
   else return array1.concat(array2);
 }
