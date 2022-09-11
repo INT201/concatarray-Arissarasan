2b690ba2ba75c0b7c9a33cefbfd56713357aa7b9
Arissarasan <cha.baasan@gmail.com>
2022-09-11 12:20:15 +0700

    Sixth

--- concatArray.js
+++ concatArray.js
@@ -3,15 +3,14 @@ const { template } = require("@babel/core");
 function concatArray(array1, array2) {
   if (
     (array1 === null || array1 === undefined) &&
-    (array1 === null || array2 === undefined)
+    (array2 === null || array2 === undefined)
   )
     return undefined;
-  else if (array1?.length === 0 && array2?.length === 0)
-    return []; // array1 และ array2 ทั้งคู่เป็น empty array ทั้งคู่ ให้ return empty array
+  else if (array1?.length === 0 && array2?.length === 0) return [];
   else if (array1 === null || array1 === undefined || array1.length === 0)
-    return array2; //array1 มีค่าเป็น empty array ,null,undefined ให้ return element ของ array2
+    return array2;
   else if (array2 === null || array2 === undefined || array2.length === 0)
-    return array1; ////array2 มีค่าเป็น empty array ,null,undefined ให้ return element ของ array1
+    return array1;
   else return array1.concat(array2);
 }
 module.exports = concatArray;
