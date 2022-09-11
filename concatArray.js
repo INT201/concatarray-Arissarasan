8fa4f1cd867f4f82480f2cfcafbe477c95eabfee
Arissarasan <cha.baasan@gmail.com>
2022-09-11 12:16:07 +0700

    Fourth

--- concatArray.js
+++ concatArray.js
@@ -8,9 +8,9 @@ function concatArray(array1, array2) {
     return undefined; // array1 และ array2 ทั้งคู่มีค่า null หรือ undefined ให้คืนค่ากลับเป็น undefined
   else if (array1?.length === 0 && array2?.length === 0)
     return []; // array1 และ array2 ทั้งคู่เป็น empty array ทั้งคู่ ให้ return empty array
-  else if (array1 === null || array1 === undefined || array1.length === [])
+  else if (array1 === null || array1 === undefined || array1.length === 0)
     return array2; //array1 มีค่าเป็น empty array ,null,undefined ให้ return element ของ array2
-  else if (array2 === null || array2 === undefined || array2.length === [])
+  else if (array2 === null || array2 === undefined || array2.length === 0)
     return array1; ////array2 มีค่าเป็น empty array ,null,undefined ให้ return element ของ array1
   else return array1.concat(array2);
 }
